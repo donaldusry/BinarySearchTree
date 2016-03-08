1d228ec7dbb0bf5c529b8357f632992e62e52f9c
Donald Usry <donald@donalds-macbook-pro.local>
2016-03-07 21:15:57 -0600

    Initial Commit

---
+++ BinarySearchTree/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BinarySearchTree
+//
+//  Created by Donald Usry on 3/7/16.
+//  Copyright © 2016 Donald Usry. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
